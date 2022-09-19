0edf63eaf3c974feb56ea4600b2897282a8f1db5
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-19 18:51:21 +0100

    jQuery-DOM-ONE

---
+++ jQuery-DOM-ONE.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jQuery-DOM</title>
+    <script src = "jquery.js"></script>
+</head>
+<body>
+    <button id="button">TimesOfSubmissions</button>
+        <div>You have Submitted <span id="Submitted">0 times.</span> </div>
+
+
+    <script>
+        var Clicks = 0;
+        function Handler () {
+            Clicks ++;
+            var numClickSpan = $('#Submitted');
+
+            if (Clicks == 1) 
+                numClickSpan.html('Once');
+            
+            else 
+                numClickSpan.html(Clicks + 'times');
+        }
+        $('#button').click(Handler);
+    </script>
+</body>
+</html>
