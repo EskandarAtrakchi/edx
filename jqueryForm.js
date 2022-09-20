972eba593bb9af8c3bc784f550cfd47839eff95b
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-20 12:42:11 +0100

    jqueryForm

--- jqueryForm.js
+++ jqueryForm.js
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jqueryForm</title>
-    <script src = "jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
     <form>
@@ -36,6 +36,9 @@
                 $('#AnimalType').html($(this).val());
             }
         });
+        //handling the check box
+        //var Checked = [];
+
     </script>
 </body>
 </html>
